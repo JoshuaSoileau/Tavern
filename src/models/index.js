7b7e02cf8823bc5d9ad95770710435b9293121c8
Joshua <jsoileau@redventures.com>
2018-10-01 17:28:17 -0400

    Fix incorrect reference

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
-import NPC from './Alchemist/NPC/NPC';
+import NPC from './NPC/NPC';
 import Town from './Town/Town';
 import Alchemist from './Alchemist/Alchemist';
 import Tavern from './Tavern/Tavern';
