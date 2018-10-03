65325ff0250d9f1a1990977e3285d2adef07d3ea
Joshua <jsoileau@redventures.com>
2018-10-02 21:49:42 -0400

    Lowercase placeholders

--- src/components/cards/TavernCard.js
+++ src/components/cards/TavernCard.js
@@ -7,9 +7,9 @@ class TavernCard extends Component {
       <div className="card">
         <h6 className="card__subtitle  delay--1">The tavern</h6>
         <h1 className="card__title  delay--2">{this.props.business.name}</h1>
-        <p className="delay--3">{`{ ENTRANCE_DESCRIPTION }`}</p>
+        <p className="delay--3">{`{ entrance_description }`}</p>
         <p className="delay--4">This is the tavern.</p>
-        <p className="delay--5">{`{ TAVERN_DESCRIPTION }`}</p>
+        <p className="delay--5">{`{ tavern_description }`}</p>
         <p className="delay--6">Owner: <Link to={`${this.props.match.url}/owner`}>{this.props.owner.name}</Link>.</p>
       </div>
     );
