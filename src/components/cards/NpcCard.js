f9b00c9010bcc4e24f85618ceab933eaf9c71bee
Joshua <jsoileau@redventures.com>
2018-10-02 21:42:02 -0400

    Template adjustments

--- src/components/cards/NpcCard.js
+++ src/components/cards/NpcCard.js
@@ -8,8 +8,8 @@ class NpcCard extends Component {
         <h6 className="card__subtitle  delay--1">NPC</h6>
         <h1 className="card__title  delay--2">{this.props.name}</h1>
         <p className="delay--3">This is a character.</p>
-        <p className="delay--4">CHARACTER_DESCRIPTION</p>
-        <p className="delay--5">CHARACTER_BACKSTORY</p>
+        <p className="delay--4">{'{ CHARACTER_DESCRIPTION }'}</p>
+        <p className="delay--5">{'{ CHARACTER_BACKSTORY }'}</p>
       </div>
     );
   }
--- src/components/cards/TownCard.js
+++ src/components/cards/TownCard.js
@@ -9,9 +9,9 @@ class TownCard extends Component {
         <h6 className="card__subtitle  delay--1">The town of</h6>
         <h1 className="card__title  delay--2">{this.props.town.name}</h1>
         <p className="delay--3">{this.props.town.description}</p>
-        <p className="delay--4">The primary road in {this.props.town.name} is ROAD_NAME. It is ROAD_DESCRIPTION.</p>
-        <p className="delay--5">You can see on the left of ROAD_NAME a tavern, with a sign inscribed with <Link to={`/tavern`}>{this.props.tavern.business.name}</Link>.</p>
-        <p className="delay--6">Further down ROAD_NAME is an alchemy lab named <Link to={`/alchemist`}>{this.props.alchemist.business.name}</Link>.</p>
+        <p className="delay--4">The primary road in {this.props.town.name} is {`{ ROAD_NAME }`}. It is {`{ ROAD_DESCRIPTION }`}.</p>
+        <p className="delay--5">You can see on the left of {`{ ROAD_NAME }`} a tavern, with a sign inscribed with <Link to={`/tavern`}>{this.props.tavern.business.name}</Link>.</p>
+        <p className="delay--6">Further down {`{ ROAD_NAME }`} is an alchemy lab named <Link to={`/alchemist`}>{this.props.alchemist.business.name}</Link>.</p>
       </div>
     );
   }
