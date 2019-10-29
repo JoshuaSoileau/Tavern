1b6ee516dce5165ca747d604e41a327c96b90e2e
Joshua Soileau <joshua.soileau@gmail.com>
2019-10-29 15:26:50 -0400

    Change reference back

--- src/models/Items/index.js
+++ src/models/Items/index.js
@@ -1,6 +1,6 @@
 import { items as armor } from "./armor/armor.json";
 import { items as weapons } from "./weapons/weapons.json";
-import { items as potions } from "./potion/potions.json";
+import { items as potions } from "./potions/potions.json";
 import { items as combat } from "./magic-items/combat.json";
 import { items as noncombat } from "./magic-items/non-combat.json";
 import { items as consumables } from "./magic-items/consumables.json";
